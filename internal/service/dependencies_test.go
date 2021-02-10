144aa94ce422cafac9b9eedfef5123b27b2bcc98
chenk keinan <chen.keinan@appsflyer.com>
2021-02-10 07:08:05 +0200

    Add tests

--- internal/service/dependencies_test.go
+++ internal/service/dependencies_test.go
@@ -5,11 +5,26 @@ import (
 	"github.com/chen-keinan/npm-dep-tree/internal/logger"
 	"github.com/chen-keinan/npm-dep-tree/pkg/model"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
+var d Dep
+
+// TestMain will exec each test, one by one
+func TestMain(m *testing.M) {
+	// exec setUp function
+	setUp()
+	// exec test and this returns an exit code to pass to os
+	retCode := m.Run()
+	os.Exit(retCode)
+}
+
+func setUp() {
+	d = NewDependencies(logger.NewZapLogger(), cache.NewLru())
+}
+
 func Test_ResolveDependenciesAddToCache(t *testing.T) {
-	d := NewDependencies(logger.NewZapLogger(), cache.NewLru())
 	dt := &model.DependencyTree{Name: "async", Version: "2.0.1", Dependencies: []*model.DependencyTree{}}
 	err := d.ResolveDependencies(dt)
 	assert.NoError(t, err)
@@ -21,7 +36,6 @@ func Test_ResolveDependenciesAddToCache(t *testing.T) {
 }
 
 func Test_ResolveDependenciesFromCache(t *testing.T) {
-	d := NewDependencies(logger.NewZapLogger(), cache.NewLru())
 	dt := &model.DependencyTree{Name: "async", Version: "2.0.1", Dependencies: []*model.DependencyTree{}}
 	err := d.ResolveDependencies(dt)
 	assert.NoError(t, err)
